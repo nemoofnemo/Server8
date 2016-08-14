3dd00e355e9e4c51367e0e95a21bec2681ee9ec0
Sheng Yu <nemoofnemo@gmail.com>
2016-08-14 09:17:53 +0800

    dev 1
    .

---
+++ Server8/svrlib.h
@@ -0,0 +1,29 @@
+#ifndef SVRLIB_H_
+#define SVRLIB_H_
+
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <time.h>  
+#include <string.h>  
+#include <process.h>
+#include <Winsock2.h>
+#include <Windows.h>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <list>
+#include <array>
+#include <vector>
+#include <hash_map>
+#include <map>
+
+#pragma comment(lib, "ws2_32.lib") 
+
+//debug mode
+#define DEBUG
+#define WIN_SVR
+#define Interface struct
+
+#endif // SVRLIB_H_
---
+++ Server8/svrutil.h
@@ -0,0 +1,286 @@
+#ifndef SVRUTIL
+#define SVRUTIL
+#include "svrlib.h"
+
+using std::string;
+
+namespace svrutil{
+	class Timer;
+	class LogModule;
+	class CriticalSection;
+	class SRWLock;
+};
+
+#pragma warning(push)
+#pragma warning(disable: 4244)
+//定时器
+//支持精度为毫秒级.错误代码0x502
+class svrutil::Timer{
+private:
+	static const int		TIMER_MODULE_ERROR = 0x502;
+	LARGE_INTEGER	large_interger;
+	LARGE_INTEGER	time;
+	double freq;
+
+public:
+	Timer(){
+		if ( ! QueryPerformanceFrequency(&large_interger)){
+			exit(TIMER_MODULE_ERROR);
+		}
+
+		freq = large_interger.QuadPart;
+	}
+
+	~Timer(){
+		//...
+	}
+
+	//start timer
+	void start(void){
+		if ( ! QueryPerformanceCounter(&time)){
+			exit(TIMER_MODULE_ERROR);
+		}
+	}
+
+	//stop timer and return internal time in milliseconds
+	unsigned long long stop(void){
+		if ( ! QueryPerformanceCounter(&large_interger)){
+			exit(TIMER_MODULE_ERROR);
+		}
+		return unsigned long long((large_interger.QuadPart - time.QuadPart) * 1000 / freq);
+	}
+
+	double stopInDouble(void){
+		if (!QueryPerformanceCounter(&large_interger)){
+			exit(TIMER_MODULE_ERROR);
+		}
+		return ((large_interger.QuadPart - time.QuadPart) * 1000 / freq);
+	}
+	
+	//todo
+	//wait for N milliseconds
+	static void wait(const unsigned long long ago){
+		Sleep(ago);
+	}
+};
+#pragma warning(pop)
+
+//带有缓冲区的日志记录模块
+//构造函数file的实参为"console"时,向控制台输出日志
+//否则写入指定文件,file为相对或绝对路径
+//错误代码0x501
+class svrutil::LogModule{
+private:
+	static const int	 DEFAULT_BUFFER_SIZE	= 0x2000;
+	static const int	 LOG_MODULE_ERROR		= 0x501;
+
+	char *		buffer;
+	int			index;
+	int			limit;
+	string		filePath;
+	FILE *		pFile;
+	CRITICAL_SECTION lock;
+
+	//not available
+	LogModule(){
+
+	}
+
+	//not available
+	LogModule(const LogModule & lm) {
+
+	}
+
+	//not available
+	void operator=(const LogModule &) {
+
+	}
+
+public:
+
+	//构造函数file的实参为"console"时,向控制台输出日志
+	//否则写入指定文件,file为相对或绝对路径
+	LogModule(const string & file, const int & bufSize = DEFAULT_BUFFER_SIZE, const string & mode = string("a")){
+		if (bufSize < 0){
+			exit(LOG_MODULE_ERROR);
+		}
+
+		filePath = file;
+		index = 0;
+		limit = bufSize;
+		buffer = NULL;
+		//pFile = NULL;
+
+		if (file == "console"){
+			pFile = stdout;
+		}
+		else if ( 0 != ::fopen_s(&pFile, file.c_str(), mode.c_str()) ){
+			exit(LOG_MODULE_ERROR);
+		}
+		else{			
+			buffer = new char[limit];
+		}
+
+		if ( ! InitializeCriticalSectionAndSpinCount(&lock, 0x1000)){
+			exit(LOG_MODULE_ERROR);
+		}
+	}
+
+	~LogModule(){
+		flush();
+		if (filePath != "console"){
+			fclose(pFile);
+			delete[] buffer;
+		}
+		DeleteCriticalSection(&lock);
+	}
+
+	//向缓冲区写入数据.如果缓冲区已满,则写入指定文件中.
+	//当路径为"console"时,不缓冲,直接输出到控制台.
+	int write(const char * str, ... ){
+		int num = 0;
+		char timeStamp[32] = "";//char timeStamp[] = "[YYYY/MM/DD-HH:MM:SS:mmmm]: ";
+
+		SYSTEMTIME systemTime;
+		GetSystemTime(&systemTime);
+		sprintf_s<32>(timeStamp, "[%04d/%02d/%02d %02d:%02d:%02d:%04d]: ", systemTime.wYear, systemTime.wMonth, systemTime.wDay, systemTime.wHour, systemTime.wMinute, systemTime.wSecond, systemTime.wMilliseconds);
+		
+		string format(timeStamp);
+		format += str;
+		format += '\n';
+
+		va_list vl;
+		va_start(vl, str);
+
+		if (filePath == "console"){
+			num = ::vfprintf_s(stdout, format.c_str(), vl);
+		}
+		else{
+			int len = format.size();
+			if (len >= limit){
+				flush();
+				num = ::vfprintf(pFile, format.c_str(), vl);
+			}
+			else{
+				if (index + len >= limit){
+					flush();
+				}
+				EnterCriticalSection(&lock);
+				num = ::vsprintf_s(buffer + index, limit - index, format.c_str(), vl);
+				index += num;
+				LeaveCriticalSection(&lock);
+			}
+		}
+		va_end(vl);
+
+		return num;
+	}
+
+	//提交缓冲区内容到物理存储器
+	void flush(void){
+		if (filePath != "console"){
+			EnterCriticalSection(&lock);
+			::fwrite(buffer, index, 1, pFile);
+			::fflush(pFile);
+			index = 0;
+			LeaveCriticalSection(&lock);
+		}
+	}
+};
+
+//critical section
+class svrutil::CriticalSection{
+private:
+	static const int CRITICAL_SECTION_ERROR = 0x503;
+	CRITICAL_SECTION lock;
+	int	refCount;
+	bool lockFlag;
+
+	CriticalSection(){
+
+	}
+
+	CriticalSection(const CriticalSection &){
+	
+	}
+
+	void operator=(const CriticalSection &){
+
+	}
+
+	bool init(DWORD dwSpinCount){		
+		bool ret = InitializeCriticalSectionAndSpinCount(&lock, dwSpinCount);
+		this->refCount = 0;
+
+		if (ret){
+			lockFlag = true;
+		}
+		else{
+			lockFlag = false;
+		}
+
+		return ret;
+	}
+
+public:
+	~CriticalSection(){
+		if (lockFlag)
+			DeleteCriticalSection(&lock);
+	}
+
+	int getRefCount(void){
+		return refCount;
+	}
+
+	bool enter(void){
+		EnterCriticalSection(&lock); 
+		refCount++;
+		return true;
+	}
+
+	bool leave(void){
+		if (refCount > 0){
+			LeaveCriticalSection(&lock);
+			refCount--;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	bool tryEnter(void){
+		bool ret = ::TryEnterCriticalSection(&lock);
+		if (!ret){
+			return false;
+		}
+		else{
+			refCount++;
+			return true;
+		}
+	}
+
+	//create a CriticalSection instance.
+	//warning:
+	//the pointer that returned by create method 
+	//must released by delete key word or calling destructor.
+	static CriticalSection * createCriticalSection(DWORD dwSpinCount = 0x00000400){
+		CriticalSection * pcs = new CriticalSection();
+		if (pcs->init(dwSpinCount) == false){
+			delete pcs;
+			return NULL;
+		}
+		else{
+			return pcs;
+		}
+	}
+};
+
+//slim read/write lock
+//(SRW_Lock in windows)
+class svrutil::SRWLock {
+
+
+};
+
+#endif // !SVRUTIL
