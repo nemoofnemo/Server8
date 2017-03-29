3c03ae029eff73dfcd1d963ddbfe5911de8bba6b
yuanyi <603035634@qq.com>
2017-03-29 19:55:09 +0800

    clu 3

--- Cluster/main.cpp
+++ Cluster/main.cpp
@@ -1,109 +1,156 @@
-#include <sys/types.h>
-#include <event2/event-config.h>
-#include <sys/stat.h>
+/*
+This exmple program provides a trivial server program that listens for TCP
+connections on port 9995.  When they arrive, it writes a short message to
+each client connection, and closes each connection once it is flushed.
 
-#ifndef WIN32
-#include <sys/queue.h>
-#include <unistd.h>
-#endif
+Where possible, it exits cleanly in response to a SIGINT (ctrl-c).
+*/
 
-#include <time.h>
 
-#ifdef _EVENT_HAVE_SYS_TIME_H
-#include <sys/time.h>
-#endif
-
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <event2/event.h>
-#include <event2/event_struct.h>
+#include <stdio.h>
+#include <signal.h>
+#ifndef WIN32
+#include <netinet/in.h>
+# ifdef _XOPEN_SOURCE_EXTENDED
+#  include <arpa/inet.h>
+# endif
+#include <sys/socket.h>
+#endif
+
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
 #include <event2/util.h>
+#include <event2/event.h>
 
-#ifdef WIN32
-#include <winsock2.h>
-#endif
+static const char MESSAGE[] = "Hello, World!\n";
 
-struct timeval lasttime;
-int event_is_persistent;
+static const int PORT = 6001;
 
-static void timeout_cb(evutil_socket_t fd, short event, void *arg)
+static void listener_cb(struct evconnlistener *, evutil_socket_t,
+	struct sockaddr *, int socklen, void *);
+static void conn_writecb(struct bufferevent *, void *);
+static void conn_readcb(struct bufferevent *, void *);
+static void conn_eventcb(struct bufferevent *, short, void *);
+static void signal_cb(evutil_socket_t, short, void *);
+
+int
+main(int argc, char **argv)
 {
-	struct timeval newtime, difference;
-	struct event *timeout = (struct event *)arg;
-	double elapsed;
-
-
-	evutil_gettimeofday(&newtime, NULL);
-	evutil_timersub(&newtime, &lasttime, &difference);
-	elapsed = difference.tv_sec +
-		(difference.tv_usec / 1.0e6);
-
-
-	printf("timeout_cb called at %d: %.3f seconds elapsed.\n",
-		(int)newtime.tv_sec, elapsed);
-	lasttime = newtime;
-
-
-	if (!event_is_persistent) {
-		struct timeval tv;
-		evutil_timerclear(&tv);
-		tv.tv_sec = 2;
-		event_add(timeout, &tv);
-	}
-}
-
-
-int main(int argc, char **argv)
-{
-	struct event timeout;
-	struct timeval tv;
 	struct event_base *base;
-	int flags;
+	struct evconnlistener *listener;
+	struct event *signal_event;
 
-
+	struct sockaddr_in sin;
 #ifdef WIN32
-	WORD wVersionRequested;
-	WSADATA wsaData;
-
-
-	wVersionRequested = MAKEWORD(2, 2);
-
-
-	(void)WSAStartup(wVersionRequested, &wsaData);
+	WSADATA wsa_data;
+	WSAStartup(0x0201, &wsa_data);
 #endif
 
-
-	if (argc == 2 && !strcmp(argv[1], "-p")) {
-		event_is_persistent = 1;
-		flags = EV_PERSIST;
-	}
-	else {
-		event_is_persistent = 0;
-		flags = 0;
-	}
-
-
-	/* Initalize the event library */
 	base = event_base_new();
+	if (!base) {
+		fprintf(stderr, "Could not initialize libevent!\n");
+		return 1;
+	}
 
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(PORT);
 
-	/* Initalize one event */
-	event_assign(&timeout, base, -1, flags, timeout_cb, (void*)&timeout);
+	listener = evconnlistener_new_bind(base, listener_cb, (void *)base,
+		LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_FREE, -1,
+		(struct sockaddr*)&sin,
+		sizeof(sin));
 
+	if (!listener) {
+		fprintf(stderr, "Could not create a listener!\n");
+		return 1;
+	}
 
-	evutil_timerclear(&tv);
-	tv.tv_sec = 2;
-	event_add(&timeout, &tv);
+	signal_event = evsignal_new(base, SIGINT, signal_cb, (void *)base);
 
-
-	evutil_gettimeofday(&lasttime, NULL);
-
+	if (!signal_event || event_add(signal_event, NULL)<0) {
+		fprintf(stderr, "Could not create/add a signal event!\n");
+		return 1;
+	}
 
 	event_base_dispatch(base);
 
+	evconnlistener_free(listener);
+	event_free(signal_event);
+	event_base_free(base);
 
-	return (0);
+	printf("done\n");
+	return 0;
+}
+
+static void
+listener_cb(struct evconnlistener *listener, evutil_socket_t fd,
+	struct sockaddr *sa, int socklen, void *user_data)
+{
+	struct event_base *base = (struct event_base *)user_data;
+	struct bufferevent *bev;
+
+	bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+	if (!bev) {
+		fprintf(stderr, "Error constructing bufferevent!");
+		event_base_loopbreak(base);
+		return;
+	}
+	bufferevent_setcb(bev, NULL, conn_writecb, conn_eventcb, NULL);
+	bufferevent_setcb(bev, NULL, conn_readcb, conn_eventcb, NULL);
+	bufferevent_enable(bev, EV_WRITE);
+	bufferevent_enable(bev, EV_READ);
+}
+
+static void conn_readcb(struct bufferevent * bev, void * user_data) {
+	struct evbuffer *input = bufferevent_get_input(bev);
+	if (evbuffer_get_length(input) == 0) {
+		printf("flushed answer\n");
+		bufferevent_free(bev);
+	}
+	else {
+		char str[256] = { 0 };
+		bufferevent_read(bev, str, 256);
+		puts(str);
+		bufferevent_write(bev, MESSAGE, strlen(MESSAGE));
+	}
+}
+
+static void
+conn_writecb(struct bufferevent *bev, void *user_data)
+{
+	struct evbuffer *output = bufferevent_get_output(bev);
+	if (evbuffer_get_length(output) == 0) {
+		printf("flushed answer\n");
+		bufferevent_free(bev);
+	}
+}
+
+static void
+conn_eventcb(struct bufferevent *bev, short events, void *user_data)
+{
+	if (events & BEV_EVENT_EOF) {
+		printf("Connection closed.\n");
+	}
+	else if (events & BEV_EVENT_ERROR) {
+		printf("Got an error on the connection: %s\n",
+			strerror(errno));/*XXX win32*/
+	}
+	/* None of the other events can happen here, since we haven't enabled
+	* timeouts */
+	bufferevent_free(bev);
+}
+
+static void
+signal_cb(evutil_socket_t sig, short events, void *user_data)
+{
+	struct event_base *base = (struct event_base *)user_data;
+	struct timeval delay = { 2, 0 };
+
+	printf("Caught an interrupt signal; exiting cleanly in two seconds.\n");
+
+	event_base_loopexit(base, &delay);
 }
